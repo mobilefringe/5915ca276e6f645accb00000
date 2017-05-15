5745fee3de4df5c3c325c4e350ba055e725320ac
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-15 14:12:20 -0400

    Orchard Town Center 2.3  /application.js

--- application.js
+++ application.js
@@ -1,46 +1,105 @@
-function renderStoreList(container, template, collection, starter, breaker){
+// function renderStoreList(container, template, collection, starter, breaker){
+//     var item_list = [];
+//     var item_rendered = [];
+//     var template_html = $(template).html();
+//     Mustache.parse(template_html);   // optional, speeds up future uses
+//     var store_initial="";
+//     $.each( collection , function( key, val ) {
+//         if(!val.store_front_url ||  val.store_front_url.indexOf('missing.png') > -1 || val.store_front_url.length === 0){
+//             val.alt_store_front_url = "";
+//         } else {
+//             val.alt_store_front_url = getImageURL(val.store_front_url);    
+//         }
+//         //var categories = getStoreCategories();
+//         var current_initial = val.name[0];
+//         val.cat_list = val.categories.join(',')
+//         if(store_initial.toLowerCase() == current_initial.toLowerCase()){
+//             val.initial = "";
+//             val.show = "display:none;";
+//         }
+//         else {
+//             val.initial = current_initial;
+//             store_initial = current_initial;
+//             val.show = "display:block;";
+//         }
+//         if (val.promotions.length > 0){
+//             val.promotion_exist = "display:inline-block";
+//         }
+//         else {
+//             val.promotion_exist = "display:none";
+//         }
+//         if (val.jobs.length > 0){
+//             val.job_exist = "display:inline-block";
+//         }
+//         else {
+//             val.job_exist = "display:none";
+//         }
+//         val.block = current_initial + '-block';
+//         var rendered = Mustache.render(template_html,val);
+//         var upper_current_initial = current_initial.toUpperCase();
+//         // if (upper_current_initial.charCodeAt(0) <= breaker.charCodeAt(0) && upper_current_initial.charCodeAt(0) >= starter.charCodeAt(0)){
+//         //     item_rendered.push(rendered);
+//         // }
+//     });
+//     $(container).show();
+//     $(container).html(item_rendered.join(''));
+// }
+
+function renderStoreList(container, template, collection, type){
     var item_list = [];
     var item_rendered = [];
     var template_html = $(template).html();
     Mustache.parse(template_html);   // optional, speeds up future uses
     var store_initial="";
-    $.each( collection , function( key, val ) {
-        if(!val.store_front_url ||  val.store_front_url.indexOf('missing.png') > -1 || val.store_front_url.length === 0){
-            val.alt_store_front_url = "";
-        } else {
-            val.alt_store_front_url = getImageURL(val.store_front_url);    
+    $.each(collection, function(key, val) {
+        if(type == "stores" || type == "category_stores"){
+            if(!val.store_front_url_abs ||  val.store_front_url_abs.indexOf('missing.png') > -1 || val.store_front_url_abs.length === 0){
+                val.store_front_url_abs = default_image.image_url;
+            } 
         }
-        //var categories = getStoreCategories();
+        
         var current_initial = val.name[0];
         val.cat_list = val.categories.join(',')
         if(store_initial.toLowerCase() == current_initial.toLowerCase()){
             val.initial = "";
             val.show = "display:none;";
-        }
-        else {
+        } else {
             val.initial = current_initial;
             store_initial = current_initial;
-            val.show = "display:block;";
+            val.show = "visibility: hidden";
         }
-        if (val.promotions.length > 0){
-            val.promotion_exist = "display:inline-block";
+        
+        if(val.is_coming_soon_store == true){
+            val.coming_soon_store = "display: block";
+        } else {
+            val.coming_soon_store = "display:none";
         }
-        else {
-            val.promotion_exist = "display:none";
+        
+        if(val.is_new_store == true){
+            val.new_store = "display: block";
+        } else {
+            val.new_store = "display: none";
         }
-        if (val.jobs.length > 0){
-            val.job_exist = "display:inline-block";
+        if(val.total_published_promos != 0){
+            val.promotion_exist = "display: inline";
+            val.promotion_list = val.total_published_promos;
+        } else {
+            val.promotion_exist = "display: none";
         }
-        else {
-            val.job_exist = "display:none";
+        
+        if (val.total_published_jobs != 0){
+            val.job_exist = "display: inline";
+            val.job_list = val.total_published_jobs;
+        } else {
+            val.job_exist = "display: none";
         }
+        
         val.block = current_initial + '-block';
         var rendered = Mustache.render(template_html,val);
         var upper_current_initial = current_initial.toUpperCase();
-        // if (upper_current_initial.charCodeAt(0) <= breaker.charCodeAt(0) && upper_current_initial.charCodeAt(0) >= starter.charCodeAt(0)){
-        //     item_rendered.push(rendered);
-        // }
+        item_rendered.push(rendered);
     });
+    
     $(container).show();
     $(container).html(item_rendered.join(''));
 }
