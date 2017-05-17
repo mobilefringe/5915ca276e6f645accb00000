8c58e325eb5f1662878e9ee5aa412d7145e8da90
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-17 16:41:13 -0400

    Orchard Town Center 2.3  /application.js

--- application.js
+++ application.js
@@ -1,50 +1,3 @@
-// function renderStoreList(container, template, collection, starter, breaker){
-//     var item_list = [];
-//     var item_rendered = [];
-//     var template_html = $(template).html();
-//     Mustache.parse(template_html);   // optional, speeds up future uses
-//     var store_initial="";
-//     $.each( collection , function( key, val ) {
-//         if(!val.store_front_url ||  val.store_front_url.indexOf('missing.png') > -1 || val.store_front_url.length === 0){
-//             val.alt_store_front_url = "";
-//         } else {
-//             val.alt_store_front_url = getImageURL(val.store_front_url);    
-//         }
-//         //var categories = getStoreCategories();
-//         var current_initial = val.name[0];
-//         val.cat_list = val.categories.join(',')
-//         if(store_initial.toLowerCase() == current_initial.toLowerCase()){
-//             val.initial = "";
-//             val.show = "display:none;";
-//         }
-//         else {
-//             val.initial = current_initial;
-//             store_initial = current_initial;
-//             val.show = "display:block;";
-//         }
-//         if (val.promotions.length > 0){
-//             val.promotion_exist = "display:inline-block";
-//         }
-//         else {
-//             val.promotion_exist = "display:none";
-//         }
-//         if (val.jobs.length > 0){
-//             val.job_exist = "display:inline-block";
-//         }
-//         else {
-//             val.job_exist = "display:none";
-//         }
-//         val.block = current_initial + '-block';
-//         var rendered = Mustache.render(template_html,val);
-//         var upper_current_initial = current_initial.toUpperCase();
-//         // if (upper_current_initial.charCodeAt(0) <= breaker.charCodeAt(0) && upper_current_initial.charCodeAt(0) >= starter.charCodeAt(0)){
-//         //     item_rendered.push(rendered);
-//         // }
-//     });
-//     $(container).show();
-//     $(container).html(item_rendered.join(''));
-// }
-
 function renderStoreList(container, template, collection, type){
     var item_list = [];
     var item_rendered = [];
@@ -139,7 +92,7 @@ function renderStoreDetails(container, template, collection, slug){
     item_list.push(collection);
     $.each( item_list , function( key, val ) {
         if ((val.store_front_url).indexOf('missing.png') > -1){
-            val.alt_store_front_url = "//codecloud.cdn.speedyrails.netssets.codecloudapp.com/sites/57f7f01f6e6f647835890000/image/png/1461352407000/HallifaxLogo.png";
+            val.alt_store_front_url = "//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
         } else {
             val.alt_store_front_url = getImageURL(val.store_front_url); 
         }
@@ -196,7 +149,7 @@ function renderPromotions(container, template, collection, centre){
             val.store_slug = "/stores/" + store_details.slug
         }
         else{
-            val.image_url = "//codecloud.cdn.speedyrails.net/sites/57f7f01f6e6f647835890000/image/png/1461163897000/Logo.png";
+            val.image_url = "//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
             val.store_name = mall_name;
             val.store_slug = "/"
             val.store_show = "display:none;";
@@ -210,14 +163,6 @@ function renderPromotions(container, template, collection, centre){
         else {
             val.description_short = val.description
         }
-        // var show_date = new Date (val.show_on_web_date + "T04:00:00Z");
-        // start = new Date (val.start_date + "T04:00:00Z");
-        // end = new Date (val.end_date + "T04:00:00Z");
-        // if (start.toDateString() == end.toDateString()) {
-        //     val.dates = (get_month(start.getMonth()))+" "+(start.getDate());    
-        // } else {
-        //     val.dates = (get_month(start.getMonth()))+" "+(start.getDate())+" - "+get_month(end.getMonth())+" "+end.getDate();    
-        // }
         
         var show_date = moment(val.show_on_web_date).tz(getPropertyTimeZone());
         var start = moment(val.start_date).tz(getPropertyTimeZone());
@@ -266,27 +211,18 @@ function renderPromoDetails(container, template, collection, centre){
         }
         else{
             val.store_name = mall_name;
-            val.store_image = "//codecloud.cdn.speedyrails.net/sites/57f7f01f6e6f647835890000/image/png/1461163897000/Logo.png";
+            val.store_image = "//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
             val.store_show = "display:none";
             val.phone_show = "display:none";
             val.show = "display:none";
         }
         val.image_url = val.promo_image_url_abs
         if(val.image_url.indexOf('missing.png') > 0){
-            val.image_url  = "//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+            val.image_url  = "//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
         }
         if(val.promo_image_url_abs.indexOf('missing.png') > -1){
             val.promo_image_show="display:none";
         }
-        // var show_date = new Date (val.show_on_web_date + "T04:00:00Z");
-        // start = new Date (val.start_date + "T04:00:00Z");
-        // end = new Date (val.end_date + "T04:00:00Z");
-    
-        // if (start.toDateString() == end.toDateString()) {
-        //     val.dates = (get_month(start.getMonth()))+" "+(start.getDate());    
-        // } else {
-        //     val.dates = (get_month(start.getMonth()))+" "+(start.getDate())+" - "+get_month(end.getMonth())+" "+end.getDate();    
-        // }
         
         var show_date = moment(val.show_on_web_date).tz(getPropertyTimeZone());
         var start = moment(val.start_date).tz(getPropertyTimeZone());
@@ -319,10 +255,10 @@ function renderEvents(container, template, collection, centre){
         }
         else {
             val.store_name = centre;
-            val.image_url = "//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+            val.image_url = "//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
         }
         if (val.image_url.indexOf('missing.png') > 0){
-            val.image_url  = "//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+            val.image_url  = "//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
         }
         if (val.description.length > 200){
             val.description_short = val.description.substring(0,200) + "...";
@@ -331,7 +267,7 @@ function renderEvents(container, template, collection, centre){
             val.description_short = val.description
         }
         if (val.event_image_url_abs.indexOf('missing.png') > -1){
-            val.event_image_url_abs="//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+            val.event_image_url_abs="//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
         }
         
         var show_date = moment(val.show_on_web_date).tz(getPropertyTimeZone());
@@ -381,7 +317,7 @@ function renderEventDetails(container, template, collection, mall_name){
         }
         else{
             val.store_name = mall_name;
-            val.store_image = "//codecloud.cdn.speedyrails.net/sites/57f7f01f6e6f647835890000/image/png/1461352407000/HallifaxLogo.png";
+            val.store_image = "//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
             val.store_show = "display:none";
             val.phone_show = "display:none";
             val.show = "display:none";
@@ -389,7 +325,7 @@ function renderEventDetails(container, template, collection, mall_name){
         val.image_url = val.event_image_url_abs
         
         if(val.image_url.indexOf('missing.png') > 0){
-            val.image_url  = "//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+            val.image_url  = "//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
         }
         
         if(val.event_image_url_abs.indexOf('missing.png') > -1){
@@ -433,9 +369,6 @@ function renderJobs(container, template, collection){
         else {
             val.description_short = val.description;
         }
-        
-        // var show_date = new Date (val.start_date + "T04:00:00Z");
-        // val.published_on = get_month(show_date.getMonth()) + " " + show_date.getDate();
         
         var show_date = moment(val.show_on_web_date).tz(getPropertyTimeZone());
         val.published_on = show_date.format("MMM D");
@@ -487,9 +420,6 @@ function renderJobDetails(container, template, collection, mall_name){
             val.store_image = "//codecloud.cdn.speedyrails.net/sites/57f7f01f6e6f647835890000/image/png/1461352407000/HallifaxLogo.png";
         }
         
-        // var show_date = new Date (val.start_date + "T04:00:00Z");
-        // val.published_on = get_month(show_date.getMonth()) + " " + show_date.getDate();
-        
         var show_date = moment(val.show_on_web_date).tz(getPropertyTimeZone());
         val.published_on = show_date.format("MMM D");
     
@@ -536,16 +466,8 @@ function renderHours(container, template, collection, type){
                     break;
             }
             if (val.open_time && val.close_time && val.is_closed == false){
-                // var open_time = new Date (val.open_time)
-                // var close_time = new Date (val.close_time)
-                // val.open_time = convert_hour(open_time);
-                // val.close_time = convert_hour(close_time);    
-                // val.h = val.open_time+ " - " + val.close_time;
-                
                 var open_time = moment(val.open_time).tz(getPropertyTimeZone());
-                
                 var close_time = moment(val.close_time).tz(getPropertyTimeZone());
-                
                 val.h = open_time.format("h:mm A") + " - " + close_time.format("h:mm A");
             } else {
                 "Closed"
@@ -556,37 +478,6 @@ function renderHours(container, template, collection, type){
         collection = []
         collection = item_list;
     }
-    // if (type == "holiday_hours") {
-    //     $.each( collection , function( key, val ) {
-    //         if (!val.store_id && val.is_holiday == true) {
-    //             holiday = new Date (val.holiday_date)
-    //             holiday.setDate(holiday.getDate()+1);
-    //             var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    //             val.formatted_date = weekdays[holiday.getDay()]+ " " + get_month(holiday.getMonth()) + " " +holiday.getDate()+ " " + holiday.getFullYear()
-    //             if (val.open_time && val.close_time && val.is_closed == false){
-    //                 var open_time = new Date (val.open_time)
-    //                 var close_time = new Date (val.close_time)
-    //                 val.open_time = convert_hour(open_time);
-    //                 val.close_time = convert_hour(close_time);    
-    //                 if (val.open_time == "0:00 AM"){
-    //                     val.open_time = "12:00 AM"
-    //                 }
-    //                  if (val.close_time == "0:00 AM"){
-    //                     val.close_time = "12:00 AM"
-    //                 }
-    //                 val.h = val.open_time+ " - " + val.close_time;
-    //             } else {
-    //                 val.h = "Closed"
-    //             }
-    //             if (val.h != "Closed"){
-    //                 item_list.push(val)
-    //             }
-    //         }
-    //     });
-    //     collection = []
-    //     collection = item_list;
-    // }
-    
     if (type == "holiday_hours") {
         $.each( collection , function( key, val ) {
             if (!val.store_id && val.is_holiday == true) {
@@ -615,36 +506,6 @@ function renderHours(container, template, collection, type){
         collection = []
         collection = item_list;
     }
-    // if (type == "closed_hours") {
-    //     $.each( collection , function( key, val ) {
-    //         if (!val.store_id && val.is_holiday == true) {
-    //             holiday = new Date (val.holiday_date + "T05:00:00Z")
-    //             var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    //             val.formatted_date = weekdays[holiday.getDay()]+ " " + get_month(holiday.getMonth()) + " " +holiday.getDate()+ " " + holiday.getFullYear()
-    //             if (val.open_time && val.close_time && val.is_closed == false){
-    //                 var open_time = new Date (val.open_time)
-    //                 var close_time = new Date (val.close_time)
-    //                 val.open_time = convert_hour(open_time);
-    //                 val.close_time = convert_hour(close_time);    
-    //                 if (val.open_time == "0:00 AM"){
-    //                     val.open_time = "12:00 AM"
-    //                 }
-    //                  if (val.close_time == "0:00 AM"){
-    //                     val.close_time = "12:00 AM"
-    //                 }
-    //                 val.h = val.open_time+ " to " + val.close_time;
-    //             } else {
-    //                 val.h = "Closed"
-    //             }
-    //             if (val.h == "Closed"){
-    //                 item_list.push(val)
-    //             }
-    //         }
-    //     });
-    //     collection = []
-    //     collection = item_list;
-    // }
-    
     if (type == "closed_hours") {
         $.each( collection , function( key, val ) {
             if (!val.store_id && val.is_holiday == true) {
@@ -697,7 +558,7 @@ function renderEventsWithImgTemplate(template_id,html_id,not_empty_section_id,em
                 } else {
                     val.show = '';
                 }
-                val.event_image_url =  "//codecloud.cdn.speedyrails.net/sites/5438407c6e6f64462d020000/d17d8c90fe9a800df686233fab0e6569/default.jpg";
+                val.event_image_url =  "//codecloud.cdn.speedyrails.net/sites/5915ca276e6f645accb00000/image/jpeg/1495053149000/orchard_default.jpg";
             }
             item_list.push(val);        
         }
