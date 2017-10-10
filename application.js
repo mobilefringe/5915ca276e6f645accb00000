5ab5c209ce87fdf2ddeb4175d24346c764754a4c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-10 10:25:36 -0400

    Orchard Town Center 2.4  /application.js

--- application.js
+++ application.js
@@ -54,31 +54,19 @@ function renderStoreList(container, template, collection, type){
             }
         }
             
-        // var current_initial = val.name[0];
-        
-        // if(store_initial.toLowerCase() == current_initial.toLowerCase()){
-        //     val.initial = "";
-        //     val.show = "display:none;";
-        // } else {
-        //     val.initial = current_initial;
-        //     store_initial = current_initial;
-        //     val.show = "visibility: hidden";
-        // }
-        
         var current_initial = val.name[0];
+        if(isInt(current_initial)){
+            current_initial = "7";
+        }
         if(store_initial.toLowerCase() == current_initial.toLowerCase()){
-            val.data_initial = current_initial;
-            store_initial = current_initial;
             val.initial = "";
             val.show = "display:none;";
         } else {
-            val.data_initial = current_initial;
             val.initial = current_initial;
             store_initial = current_initial;
-            val.show = "display:block;";
+            val.show = "visibility: hidden";
         }
-        
-        
+
         if(val.is_coming_soon_store == true){
             val.coming_soon_store = "display: block";
         } else {
