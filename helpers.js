ab5aa0be0a46588d2b164d64b451765abf1b5cec
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-20 16:01:38 -0400

    Orchard Town Center 1.5  /helpers.js

--- helpers.js
+++ helpers.js
@@ -17,7 +17,7 @@ function init(e){
     
     $('#menu-icon').click(function(){
 		$(this).toggleClass('open');
-		$('.mobile_menu_container').addClass('active_menu');
+		$('.mobile_menu_container').toggleClass('active_menu');
 		$('body').toggleClass('no_scroll');
 	});
 	
