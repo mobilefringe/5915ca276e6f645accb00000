3977f8100213cb236926ae1cb80fbf00173fe796
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-05 14:24:19 -0400

    Orchard Town Center 1.2  /helpers.js

--- helpers.js
+++ helpers.js
@@ -646,7 +646,7 @@ function renderInstaFeed(container, template){
     var template_html = $(template).html();
     var feed_obj = {}
     Mustache.parse(template_html); 
-    $.getJSON("https://devonshire.mallmaverick.com/api/v4/devonshire/social.json").done(function(data) {
+    $.getJSON("//orchardtown.mallmaverick.com/api/v4/orchardtown/social.json").done(function(data) {
         var insta_feed = data.social.instagram
         $.each(insta_feed, function(i,v){
             if(v.caption != null){
