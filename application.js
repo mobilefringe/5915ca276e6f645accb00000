8959056fbdb713bf7a6631160dc99bddb5161dd4
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-28 10:16:51 -0400

    Orchard Town Center 1.4  /application.js

--- application.js
+++ application.js
@@ -118,6 +118,7 @@ function renderContest(container, template, collection){
     Mustache.parse(template_html);   // optional, speeds up future uses
 
     collection.alt_photo_url = getImageURL(collection.photo_url);
+    console.log(collection.alt_photo_url)
     collection.property_name = getPropertyDetails().name;
     var rendered = Mustache.render(template_html,collection);
     item_rendered.push(rendered);
